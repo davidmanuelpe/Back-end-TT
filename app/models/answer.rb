e55311bb8369c666fb2ad5d891c39d6ca8f99f4f
David Manuel <davidmanuelpe@gmail.com>
2022-02-20 10:42:10 -0300

    finalizado

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,21 @@ class Answer < ApplicationRecord
   belongs_to :formulary
   belongs_to :question
   belongs_to :visit
+  validate :status_visita
+  validates :content, presence: {message: "não pode ser vazio"}
+
+
+  def status_visita
+
+    if visit_id?
+      visita = Visit.find(visit_id)
+      if visita.status.downcase.eql?('pendente')
+        errors.add(:visita_id, "a visita ainda não aconteceu, não pode ainda responder")
+      end
+      if visita.status.downcase.eql?('realizado')
+        errors.add(:visita_id, "a visita já aconteceu, não pode mais responder")
+      end
+    end
+
+  end
 end
